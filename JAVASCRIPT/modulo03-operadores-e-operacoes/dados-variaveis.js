b86da1caa9931dc634cd4a19615c678d2bc10d86
Lasla <lasladandara@gmail.com>
2025-08-06 18:35:33 -0300

    Módulo 03 - Operadores e Operações

---
+++ JAVASCRIPT/modulo03-operadores-e-operacoes/dados-variaveis.js
@@ -0,0 +1,45 @@
+// EXERCÍCIOS DE JS HASHTAG TREINAMENTOS
+// DADOS E VARIAVEIS - MÓDULO 3
+
+// 1 - Escreva um programa simples que exiba uma string no terminal utilizando o console.log
+console.log('Bem-vindo ao Curso de JS da Hashtag&quot');
+
+// 2 - Modifique o programa anterior para armazenar a mensagem em uma variável antes de exibi-la no console.
+let mensagem = 'Iniciando em JS 2025';
+console.log(mensagem);
+
+// 3 - Declare 3 variáveis mutáveis: nome (string), idade (number) e isStudent (boolean)
+let nome = "Maria";
+let idade = 27;
+let isStudent = true;
+
+
+// 4 - Modifique o valor das variáveis anteriores e imprima os novos valores no console
+nome = "Lucas";
+idade = 29;
+isStudent = false;
+
+console.log(nome, idade, isStudent);
+
+// 5 - Crie uma variável constante que irá armazenar um número e imprima no console
+const valorProduto = 50;
+console.log(valorProduto);
+
+// 6 - Declare duas variáveis, endereço e telefone, sem atribuir valores a elas
+let endereco;
+let telefone;
+console.log(endereco);
+console.log(telefone);
+
+// 7 - Declare variáveid com nomes descritivos para armazenar o nome de um produto, seu preço e quantidade em estoque.
+let equipamentoBancada = "Bancada de Ajuste";
+let precoBancada = 48.000;
+let qtdBancada =8;
+console.log(equipamentoBancada);
+console.log(precoBancada);
+console.log(qtdBancada);
+
+// 8 - Declare uma variável para armazenar o nome de uma cidade e outra para armazenar a sua população
+let cidade = "Manaus";
+let populacao = 50000;
+console.log(`A cidade de ${cidade} tem ${populacao}`);
